56543810a21a03fb34db40d12f5011e421eb90c7
Rob Saidov <151962348+RobSaidov@users.noreply.github.com>
2024-12-07 10:04:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
+// Smooth scrolling function
+function smoothScroll(target) {
+  const element = document.querySelector(target);
+  if (element) {
+    window.scrollTo({
+      top: element.offsetTop,
+      behavior: "smooth",
+    });
+  }
+}
+
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
